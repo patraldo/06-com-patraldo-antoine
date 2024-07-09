b9693a527f4863d6c43474033048492d8f026444
Chef <cheftech@patraldo.com>
2024-07-08 21:31:03 -0600

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,73 @@
 <h1>Welcome to Antoine Patraldo</h1>
 
+<style>
 
-img src="https://maison.patraldo.com/ExGrisiJunio.jpg" alt="some description" onclick="window.open(this.src, '_blank')"
+
+@import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
+
+
+
+h1 {
+	font-family: 'Special Elite', cursive;
+font-size: 3.0rem;
+padding: 20px 20px 60px;
+  max-width: 680px;
+  margin: 0 auto;
+
+
+}
+
+
+* {
+	box-sizing: border-box;
+}
+
+.row {
+display: -ms-flexbox; /* IE10 */
+display: flex;
+	 -ms-flex-wrap: wrap; /* IE10 */
+	 flex-wrap: wrap;
+padding: 0 4px;
+}
+
+/* create four equal columns that sits next to each other */
+.column {
+	-ms-flex: 25%; /* IE10 */
+flex: 25%;
+      max-width: 25%;
+padding: 0 4px;
+}
+
+.column img {
+	margin-top: 8px;
+	vertical-align: middle;
+width: 100%;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 800px) {
+	.column {
+		-ms-flex: 50%;
+flex: 50%;
+      max-width: 50%;
+	}
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+	.column {
+		-ms-flex: 100%;
+flex: 100%;
+      max-width: 100%;
+	}
+}
+</style>
+
+
+<div class="row">
+<div class="column"><img src="https://maison.patraldo.com/ExGrisiJunio.jpg" alt="some description" onclick="window.open(this.src, '_blank')"></div>
+<div class="column"><img src="https://maison.patraldo.com/ExGrisiJunio.jpg" alt="some description" onclick="window.open(this.src, '_blank')"></div>
+<div class="column"><img src="https://maison.patraldo.com/ExGrisiJunio.jpg" alt="some description" onclick="window.open(this.src, '_blank')"></div>
+<div class="column"><img src="https://maison.patraldo.com/ExGrisiJunio.jpg" alt="some description" onclick="window.open(this.src, '_blank')"></div>
+</div> 
 
